e65a98e11232260eda91a3bbe387ee25cb565072
berkealemdaroglu <ersinberkealemdaroglu@gmail.com>
2022-10-01 17:02:35 +0300

    Coroutine sample.

--- app/src/main/java/com/ersinberkealemdaroglu/berkealemdarogluweek6/MainActivity.kt
+++ app/src/main/java/com/ersinberkealemdaroglu/berkealemdarogluweek6/MainActivity.kt
@@ -2,21 +2,13 @@ package com.ersinberkealemdaroglu.berkealemdarogluweek6
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.strictmode.CredentialProtectedWhileLockedViolation
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.ersinberkealemdaroglu.berkealemdarogluweek6.databinding.ActivityMainBinding
 import com.ersinberkealemdaroglu.berkealemdarogluweek6.di.NetworkResponse
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    var isFinish = MutableLiveData<Boolean>()
-    private val scopeIO = CoroutineScope(Job() + Dispatchers.IO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,14 +19,13 @@ class MainActivity : AppCompatActivity() {
         coroutineScope()
     }
 
-    private fun infiniteLoop(){
-        while (true){
-            for (i in 0 until Int.MAX_VALUE){
+    private fun infiniteLoop() {
+        while (true) {
+            for (i in 0 until Int.MAX_VALUE) {
                 binding.textView3.text = i.toString()
             }
         }
     }
-
 
     /**
      * Sunucudan gelen veriyi io dispatchers ile çağırdığımızda veriler main thread i bloklamadan,
@@ -45,7 +36,6 @@ class MainActivity : AppCompatActivity() {
      * sonsuz döngüyü coroutine dışına alırsak uygulama hiç açılmayacaktır. Kullanıcı Sürekli beyaz ekranda bekleyecektir. Kullanıcı sadece beyaz bir ekran görecektir.
      * Coroutine içine eklersek main de bulunan ui tasarımlarını kullanıcı görecektir ama etkileşime geçemeyecektir.
      */
-
     private fun coroutineScope() {
         CoroutineScope(Dispatchers.IO).launch {
             infiniteLoop()
@@ -59,7 +49,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
     }
 
 }
